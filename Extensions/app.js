7248a0f66528fb2b8448cde81c5ededbde60dfbf
Melanie Macken <m.macken@otmail.nl>
2018-03-04 14:35:45 +0100

    m

--- Extensions/app.js
+++ Extensions/app.js
@@ -1,20 +1,4 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 
 
-console.log("hello");
+
+console.log("hello, I wonder what brings you to the console, if you have any questions feel free to contact me!");
